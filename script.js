8a881ac895875374e0ba135fbe7e25108c993503
ncdesign90 <ncdesign90@gmail.com>
2021-05-29 21:19:36 +0000

    saving final project

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+
+
+#Compute interest rate and display message
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var message = "If you deposit " + principal + ",\n at an interest rate of " + rate + ".\nYou will receive an amount of " + interest + ",\nin the year " + year
+    document.getElementById("result").innerText=message;
 }
-        
+
+#Update rate based on slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+#Validate that principal value is a positive number
+function validatePrincipal() {
+  var checkInput = document.getElementById("pricipal");
+  if (checkInput <= "0") {
+    alert("Enter a positive number");
+    return false;
+  }
+}
