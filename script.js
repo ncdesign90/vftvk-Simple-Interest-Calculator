048fd35ecbd69941e01a8e0291c5ae273cfd388a
ncdesign90 <ncdesign90@gmail.com>
2021-05-30 14:49:49 +0000

    fixed validation

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-//Validate that principal value is a positive number
-function validatePrincipal() {
-  var checkInput = document.getElementById("pricipal").value;
-  if (checkInput <= "0") {
-    alert("Enter a positive number");
-    return false;
-  }
-}
-
-//Update rate based on slider
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}
-
 //Compute interest rate and display message
 function compute()
 {
@@ -24,4 +8,24 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var message = "If you deposit <mark>" + principal + "</mark>,<br /> at an interest rate of <mark>" + rate + "</mark>.<br />You will receive an amount of <mark>" + interest + "</mark>,<br />in the year <mark>" + year + "</mark><br /><br />";
     document.getElementById("result").innerHTML=message;
+//Set focus back to principal box once computed
+    document.getElementById("principal").focus();
+}
+
+//Update rate based on slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+//Validate that principal value is a positive number
+function validatePrincipal() {
+    var principal = document.getElementById("principal");
+    if(principal.value > 0)
+     {return false;}
+    else{
+        alert("Enter a positive number");
+    }
+
 }
