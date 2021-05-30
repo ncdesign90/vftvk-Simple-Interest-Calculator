bc3d1238ad071892b704b10ee28bd4a1a99e2398
ncdesign90 <ncdesign90@gmail.com>
2021-05-30 14:28:55 +0000

    formatting and spelling

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-//Compute interest rate and display message
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var message = "If you deposit <mark>" + principal + "</mark>,<br /> at an interest rate of <mark>" + rate + "</mark>.<br />You will receive an amount of <mark>" + interest + "</mark>,<br />in the year <mark>" + year + "</mark>";
-    document.getElementById("result").innerHTML=message;
+//Validate that principal value is a positive number
+function validatePrincipal() {
+  var checkInput = document.getElementById("pricipal").value;
+  if (checkInput <= "0") {
+    alert("Enter a positive number");
+    return false;
+  }
 }
 
 //Update rate based on slider
@@ -17,11 +14,14 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 }
 
-//Validate that principal value is a positive number
-function validatePrincipal() {
-  var checkInput = document.getElementById("pricipal");
-  if (checkInput <= "0") {
-    alert("Enter a positive number");
-    return false;
-  }
+//Compute interest rate and display message
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var message = "If you deposit <mark>" + principal + "</mark>,<br /> at an interest rate of <mark>" + rate + "</mark>.<br />You will receive an amount of <mark>" + interest + "</mark>,<br />in the year <mark>" + year + "</mark><br /><br />";
+    document.getElementById("result").innerHTML=message;
 }
